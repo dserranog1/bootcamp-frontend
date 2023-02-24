92c4b5f4954846f2e221d3b1fc2058065c7ec057
Daniel Serrano <dserranog@unal.edu.co>
2023-02-23 19:12:34 -0500

    feat: add login page with form

--- actividad-3/src/App.tsx
+++ actividad-3/src/App.tsx
@@ -1,13 +1,26 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import AuthRouter from "./auth/AuthRouter";
 
 export default function App() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="auth/*" element={<AuthRouter />} />
       </Routes>
     </main>
   );
 }
+
+const Home = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-4 bg-blue-200 text-3xl">
+      You are in the home page!
+      <Link to="auth/login">
+        <p className="w-fit rounded-md border-2 border-slate-900 p-3 hover:bg-red-300">
+          Go to Login
+        </p>
+      </Link>
+    </div>
+  );
+};
--- actividad-3/src/auth/LoginPage.tsx
+++ actividad-3/src/auth/LoginPage.tsx
@@ -1,8 +1,45 @@
+import { Card } from "antd";
+import { Field, Form, Formik } from "formik";
+
 const LoginPage = () => {
   return (
-    <div className="w-screen text-center text-2xl">
+    <div className="flex h-screen flex-row items-center justify-center text-3xl">
       {" "}
-      You are in the login page!
+      <div className="flex flex-col gap-4">
+        You are in the login page!{" "}
+        <Card
+          title="Please enter your credentials"
+          className="w-80"
+          bordered={false}
+          actions={[
+            <button type="submit" key="submitButton">
+              Login
+            </button>,
+          ]}
+        >
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={(values) => {
+              console.log(values);
+            }}
+          >
+            <Form className="flex flex-col gap-3">
+              <div>
+                <label htmlFor="email" className="mr-3">
+                  Email
+                </label>
+                <Field id="email" name="email" type="email" />
+              </div>
+              <div>
+                <label htmlFor="password" className="mr-3">
+                  Password
+                </label>
+                <Field id="password" name="password" type="password" />
+              </div>
+            </Form>
+          </Formik>
+        </Card>
+      </div>
     </div>
   );
 };
